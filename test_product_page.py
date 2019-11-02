508d8891612591cd68a019b537c3e84ca600f102
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-11-02 17:53:20 +0300

    prepare to review

--- test_product_page.py
+++ test_product_page.py
@@ -7,78 +7,101 @@ from pages.main_page import MainPage
 
 import time
 
-# link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+         pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+                      "/?promo=offer7", marks=pytest.mark.xfail),
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_be_buy_product()
-#     time.sleep(300)
+@pytest.mark.need_review
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_buy_product()
 
-# @pytest.mark.xfail(reason="success message should not be -its ok")
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_basket()
-#     page.should_not_be_success_message()
-#
-#
-# def test_guest_cant_see_success_message(browser):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_not_be_success_message()
-#
-#
-# @pytest.mark.xfail(reason="success message should not dessappared -its ok")
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_basket()
-#     page.is_disappeared()
+
+@pytest.mark.xfail(reason="success message should not be -its ok")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="success message should not dissappeared -its ok")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.is_disappeared()
+
 
 @pytest.mark.user
 class TestUserAddToBasketFromProductPage:
 
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser,
-                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        self.link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser,
+                         self.link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-        login_page = LoginPage(browser, browser.current_url)
-        login_page.register_new_user(email=str(time.time()) + "@fakemail.org", password=str(time.time()))
-        login_page.should_be_authorized_user()
+        page.register_new_user(email=str(time.time()) + "@fakemail.org", password=str(time.time()))
+        page.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_buy_product()
 
 
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+# @pytest.mark.need_review
+# def test_guest_can_add_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_buy_product()
+
+
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -86,6 +109,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
